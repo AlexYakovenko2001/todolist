a1f9d03e9df1618a52137987c029290c40d76045
AlexYakovenko2001 <mrberrington2001@gmail.com>
2022-07-25 15:17:39 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ function  App() {
         { id: 2, title: "JS", isDone: true },
         { id: 3, title: "ReactJS", isDone: false }
     ]
+
+
     const tasks2 = [
         { id: 1, title: "Hello world", isDone: true },
         { id: 2, title: "I am Happy", isDone: false },
